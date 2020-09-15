1519e4d9a9ee7aa8429384230d3c55d80dccf705
MusharratTabassum <39344758+MusharratTabassum@users.noreply.github.com>
2020-09-16 03:06:18 +0600

    Add files via upload

---
+++ Rainbow_Dash,Fluttershy_and_Chess Coloring.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int t,a;
+    cin>>t;
+    for(int i=1; i<=t; i++)
+    {
+        cin>>a;
+        cout<<(a+2)/2<<endl;
+    }
+ 
+    return 0;
+ 
+}
+ 
