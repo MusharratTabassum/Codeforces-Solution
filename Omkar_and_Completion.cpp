e30a89475db9fe526107a31083b5038156591277
MusharratTabassum <39344758+MusharratTabassum@users.noreply.github.com>
2020-09-16 03:04:10 +0600

    Add files via upload

---
+++ Omkar_and_Completion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long int a,t;
+    cin>>t;
+    for(int i=1; i<=t; i++)
+    {
+        cin>>a;
+        for(int j=1; j<=a; j++)
+        {
+            cout<<6<<" ";
+        }
+        cout<<endl;
+    }
+ 
+    return 0;
+ 
+}
+ 
